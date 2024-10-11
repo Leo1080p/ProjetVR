72c8520cca689526480b396608b5d32b934b97e1
Adelie BRUNEL <s2104315@ensimlan.univ-lemans.fr>
2024-10-11 09:55:40 +0200

    bonjour

---
+++ Assets/DangerZone.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerZone : MonoBehaviour
+{
+    [SerializeField]
+    private Color baseObjColor;
+    [SerializeField]
+    private Renderer objRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        objRenderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        objRenderer.material.color = Color.red;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        objRenderer.material.color= baseObjColor;
+    }
+}
